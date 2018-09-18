cd59bc914325f4b4ec44630f7a740bdcf12660f1
MahaAmin <mahaamin97@gmail.com>
2018-09-18 05:22:50 +0200

    Add Motifs()

--- Motifs.py
+++ Motifs.py
@@ -100,5 +100,73 @@ def GreedyMotifSearch(Dna, k, t):
 
     return BestMotifs
 
-DNA = ["GGCGTTCAGGCA", "AAGAATCAGTCA", "CAAGGAGTTCGC", "CACGTCAATCAC", "CAATAATATTCG"]
-print(GreedyMotifSearch(DNA, 3, 5))
+
+def CountWithPseudocounts(Motifs):
+    count = {}
+    k = len(Motifs[0])
+    for symbol in "ACGT":
+        count[symbol] = []
+        for j in range(k):
+            count[symbol].append(1)
+
+    t = len(Motifs)
+    for i in range(t):
+        for j in range(k):
+            symbol = Motifs[i][j]
+            count[symbol][j] += 1
+    return count
+
+
+def ProfileWithPseudocounts(Motifs):
+    counts = CountWithPseudocounts(Motifs)
+    t = len(Motifs) + 4
+    k = len(Motifs[0])
+    profile = {}
+    for symbol in "ACGT":
+        profile[symbol] = []
+        for j in range(len(counts[symbol])):
+            profile[symbol].append((counts[symbol][j] / t))
+
+    return profile
+
+
+def GreedyMotifSearchWithPseudocounts(Dna, k, t):
+    BestMotifs = []
+    for i in range(0, t):
+        BestMotifs.append(Dna[i][0:k])
+
+    n = len(Dna[0])
+    for i in range(n-k+1):
+        Motifs = []
+        Motifs.append(Dna[0][i:i+k])
+        for j in range(1, t):
+            P = ProfileWithPseudocounts(Motifs[0:j])
+            Motifs.append(ProfileMostProbablePattern(Dna[j], k, P))
+
+        if Score(Motifs) < Score(BestMotifs):
+            BestMotifs = Motifs
+
+    return BestMotifs
+
+
+def Motifs(Profile, DNA):
+    best_kmers = []
+    t = len(DNA)
+
+    for i in range(t):
+        k = len(DNA[i])
+        kmer = DNA[i][0:4]
+        best_prob = Pr(kmer, Profile)
+        for j in range(1,k-3):
+            if Pr(DNA[i][j:j+4], Profile) > best_prob:
+                best_prob = Pr(DNA[i][j:j+4], Profile)
+                kmer = DNA[i][j:j+4]
+        best_kmers.append(kmer)
+
+    return best_kmers
+
+
+ProfileTestCase0= { 'A': [0.8, 0.0, 0.0, 0.2 ],'C': [ 0.0, 0.6, 0.2, 0.0], 'G': [ 0.2 ,0.2 ,0.8, 0.0], 'T': [ 0.0, 0.2, 0.0, 0.8]}
+DnaTC0=['TTACCTTAAC','GATGTCTGTC','ACGGCGTTAG','CCCTAACGAG','CGTCAGAGGT']
+
+print(Motifs(ProfileTestCase0, DnaTC0))
